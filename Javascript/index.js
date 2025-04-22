05831ad3df7bf1278c21cf1234b93127ef533867
Nitish-786 <nitishknitish777@gmail.com>
2025-04-22 17:59:01 +0530

    commit

--- Javascript/index.js
+++ Javascript/index.js
@@ -6,6 +6,80 @@
 
 
 //comparison operator
-let age = 18;
-console.log(age > 18); //false
-console.log(age >= 18);
+// let age = 18;
+// console.log(age > 18); //false
+// console.log(age >= 18);
+
+//if statement
+// console.log("before my if statement");
+// let age = 23;
+// if (age >=18) {
+//     console.log("you an vote");
+//     console.log("you can drive");
+//     let a = 5;
+//     console.log(5 * a);
+       
+// }
+// if (age >= 20) {
+//     console.log("you are in 20s");
+    
+// }
+// console.log("after my if statement");
+
+////else if statement
+
+// let age = 14;
+// if (age >=18) {
+//     console.log("you an vote");
+       
+// }
+// else if( age >= 18 ){
+//     console.log("you cannot vote");
+    
+// }
+// else if (age < 18 ) {
+//     console.log("you cannot vote2");
+    
+// }
+
+let marks = 33;
+if (marks >= 80) {
+console.log("your grade is A+");
+}
+else if (marks>= 60) {
+console.log("your grade is A");
+}
+else if (marks >=33) {
+console.log("your grade is B");
+}
+else if (marks < 33) {
+console.log("you get fail");
+
+}
+
+//else statement
+let age = 10;
+if (age >=18) {
+    console.log("you can vote");
+}
+else{
+    console.log("you cannot vote");
+    
+}
+
+let color = "white";
+if (color=="red") {
+console.log("stop light color is red");
+}
+if (color== "yellow ") {
+console.log("slow down light color is yellow");
+
+}
+if (color=="green ") {
+console.log("go light color is green");
+
+}
+else {
+    console.log("traffic light is broken");
+    
+}
---
+++ practice01.js
@@ -0,0 +1,12 @@
+let color = "red";
+if (color=="red") {
+console.log("stop light color is red");
+}
+if (color== "yellow ") {
+console.log("slow down light color is yellow");
+
+}
+if (color=="green ") {
+console.log("go light color is green");
+
+}
