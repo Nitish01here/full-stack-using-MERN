6fec263ed3b3f344d4d84c7ec77b2af0fcf46b11
Nitish-786 <nitishknitish777@gmail.com>
2025-04-22 16:56:19 +0530

    commit

---
+++ Javascript/index.js
@@ -0,0 +1,11 @@
+// //let pencilPrice = 10;
+// let eraserPrice = 5;
+// //let output = "the total price is: " + (pencilPrice + eraserPrice) +" rupees ";
+// //let output = `the total price is : ${pencilPrice + eraserPrice} rupees`;
+// console.log(`the total price is : ${pencilPrice + eraserPrice} rupees`);
+
+
+//comparison operator
+let age = 18;
+console.log(age > 18); //false
+console.log(age >= 18);
